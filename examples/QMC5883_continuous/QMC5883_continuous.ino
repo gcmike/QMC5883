181187aed3606489d9efc3656a99e3e38a7efd9e
gcmike <gcmichael8044@gmail.com>
2017-06-13 09:30:54 +0800

    Add files via upload

---
+++ examples/QMC5883_continuous/QMC5883_continuous.ino
@@ -0,0 +1,22 @@
+#include<Wire.h>
+#include<QMC5883.h>
+
+QMC5883 qq;
+
+void setup() {
+  Serial.begin(9600);
+  qq.begin();
+}
+
+void loop() {
+  delay(200);
+  qq.calculate();
+  Serial.print("x ");
+  Serial.print(qq.getX());
+  Serial.print(", ");
+  Serial.print("y ");
+  Serial.print(qq.getY());
+  Serial.print(", ");
+  Serial.print("z ");
+  Serial.println(qq.getZ());
+}
